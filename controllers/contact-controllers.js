46d41ee2bf0aee0c010bf65473f1f05695a99b88
MAYANK CHETAN PARVATIA <104015658+mayank2368@users.noreply.github.com>
2023-03-09 10:14:39 +0100

    Created the error handling constants and imported the async error handler lib

--- controllers/contact-controllers.js
+++ controllers/contact-controllers.js
@@ -1,50 +1,50 @@
+const asyncHandler = require("express-async-handler");
+
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access public
 
-const getContact = (req, res) => {
+const getContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get all contacts" });
-};
+});
 
 //@desc Get a particular contact
 //@route GET /api/contacts/:id
 //@access public
 
-const getAContact = (req, res) => {
+const getAContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `get contact of ${req.params.id}` });
-};
+});
 
 //@desc Create a contact
 //@route POST /api/contacts/:id
 //@access public
 
-const createContact = (req, res) => {
+const createContact = asyncHandler(async (req, res) => {
   console.log("The request body is:", req.body);
   const { name, email, phone } = req.body;
   if (!name || !email || !phone) {
     res.status(400);
     throw new Error("All fields are mandatory");
-  } else {
-    res.status(200);
   }
   res.status(200).json({ message: "Create contacts" });
-};
+});
 
 //@desc Update a contact
 //@route PUT /api/contacts/:id
 //@access public
 
-const updateContact = (req, res) => {
+const updateContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `update contact for ${req.params.id}` });
-};
+});
 
 //@desc Delete a contact
 //@route DELETE /api/contacts/:id
 //@access public
 
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `delete contact for ${req.params.id}` });
-};
+});
 
 module.exports = {
   getContact,
--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,3 +1,46 @@
-const errorHandler = (err, req, res, next) => {};
+const { constants } = require("../constants");
+
+const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode ? res.statusCode : 500;
+  switch (statusCode) {
+    case constants.VALIDATION_ERROR_:
+      res.json({
+        title: "Validation failed",
+        message: err.message,
+        stacktrace: err.stack,
+      });
+
+    case constants.NOT_FOUND:
+      res.json({
+        title: "Not found",
+        message: err.message,
+        stacktrace: err.stack,
+      });
+
+    case constants.UNAUTHORIZED:
+      res.json({
+        title: "UNAUTHORIZED",
+        message: err.message,
+        stacktrace: err.stack,
+      });
+
+    case constants.FORBIDDEN:
+      res.json({
+        title: "FORBIDDEN",
+        message: err.message,
+        stacktrace: err.stack,
+      });
+
+    case constants.SERVER_ERROR:
+      res.json({
+        title: "FORBIDDEN",
+        message: err.message,
+        stacktrace: err.stack,
+      });
+    default:
+      console.log("Nein erroe, Alles Gut!!!");
+      break;
+  }
+};
 
 module.exports = errorHandler;
